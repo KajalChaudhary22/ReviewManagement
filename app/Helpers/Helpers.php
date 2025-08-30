694e2063181543f7c488523cff86d3e6ec0a8945
aarti99698 <110008975+aarti99698@users.noreply.github.com>
2025-08-30 19:22:47 +0530

    Inquries complete

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -8,21 +8,25 @@ class Helpers
 {
     public static function showStatus($status): string
     {
-        $status = ucfirst(strtolower($status));
+        // $status = ucfirst(strtolower($status));
         // dd($status);
         switch ($status) {
             case 'Active':
-                return '<span class="status-badge status-active">Active</span>';
+            case 'In Progress':
+                return '<span class="status in-progress">'.$status.'</span>';
             case 'Pending':
-                return '<span class="status-badge status-pending">Pending</span>';
+                return '<span class="status pending">'.$status.'</span>';
+            case 'Success':
+            case 'Completed':
+                return '<span class="status completed">'.$status.'</span>';
             case 'Suspended':
-                return '<span class="status-badge status-suspended">Suspended</span>';
+                return '<span class="status suspended">'.$status.'</span>';
             case 'Inactive':
-                return '<span class="status-badge status-inactive">Inactive</span>';
-                case '':
-                    return '<span class="status-badge status-pending">'.$status.'</span>';
+                return '<span class="status inactive">'.$status.'</span>';
+            case '':
+                    return '<span class="status pending">'.$status.'</span>';
             default:
-                return '<span class="status-badge status-pending">Unknown</span>';
+                return '<span class="status pending">'.$status.'</span>';
         }
     }
     public static function statusActionButton($id, string $status = 'Pending'): string
